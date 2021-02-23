00ef490b9b2f6573924e8b62ab84bd1fc0ddfcbb
qingyue.ke <qingyue.ke@mobvista.com>
2021-02-24 00:49:29 +0800

    helm form

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import 'animate.css/source/animate.css'
 import Plugins from '@/plugins'
 import {initI18n} from '@/utils/i18n'
 import bootstrap from '@/bootstrap'
-import VueCodemirror from 'vue-codemirror'
 
 const router = initRouter(store.state.setting.asyncRoutes)
 const i18n = initI18n('CN', 'US')
@@ -19,7 +18,6 @@ Vue.use(Antd)
 Vue.config.productionTip = false
 Vue.use(Viser)
 Vue.use(Plugins)
-Vue.use(VueCodemirror)
 
 bootstrap({router, store, i18n, message: Vue.prototype.$message})
 
--- src/pages/form/k8sYamlCheck2/K8sCheckForm.vue
+++ src/pages/form/k8sYamlCheck2/K8sCheckForm.vue
@@ -9,6 +9,16 @@
           :remove="handleDownloadFileRemove"
           :customRequest="downloadFilesCustomRequest"
           action="https://www.mocky.io/v2/5cc8019d300000980a055e76">
+          <p class="ant-upload-drag-icon">
+            <a-icon type="inbox" />
+          </p>
+          <p class="ant-upload-text">
+            Click or drag file to this area to upload
+          </p>
+          <p class="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+            band files
+          </p>
         </a-upload-dragger>
       </a-form-item>
       <a-row class="form-row" :gutter="5">
