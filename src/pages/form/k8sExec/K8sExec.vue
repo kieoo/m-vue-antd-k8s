82a78e35770db5ea264a6c3d65439187a7a763db
qingyue.ke <qingyue.ke@mobvista.com>
2023-04-24 20:26:44 +0800

    简化输出

--- src/pages/form/k8sExec/K8sExec.vue
+++ src/pages/form/k8sExec/K8sExec.vue
@@ -50,7 +50,7 @@
                   placeholder="please select exec namespace and owner"
                   @change="handleNsOwnChange">
           <a-select-option v-for="n in this.selectNsAndOwnerList" :key="n">
-            {{ n.split("/")[1] }}/{{ n.split("/")[2] }}/{{n.split("/")[3]}}
+            {{n.split("/")[3]}}
           </a-select-option>
         </a-select>
       </a-form-model-item>
@@ -59,7 +59,7 @@
                   mode="multiple"
                   placeholder="please select exec container">
           <a-select-option v-for="i in this.selectContainers" :key="i.con">
-            {{ i.con.split("/")[1] }}/{{ i.con.split("/")[2] }}/{{i.con.split("/")[3]}}/{{i.con.split("/")[4]}} <span style='color:indianred'>({{i.pod.length}})</span>
+            {{i.con.split("/")[3]}}/{{i.con.split("/")[4]}} <span style='color:indianred'>({{i.pod.length}})</span>
           </a-select-option>
         </a-select>
       </a-form-model-item>
